993ce418f8917425082aa421f10c57cacf3be3ff
Swen <s.mulderij@labdigital.nl>
2026-02-05 10:17:36 +0100

    commit

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -2,5 +2,16 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    servers = {
+      yamlls = {
+        settings = {
+          yaml = {
+            format = {
+              singleQuote = true,
+            },
+          },
+        },
+      },
+    },
   },
 }
